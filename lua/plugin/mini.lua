301d0fb50addb2831dacf0592ee5c151fdca2d42
khanhfg <nphuyanh@gmail.com>
2023-10-08 23:35:10 -0300

    More mini plugin

--- lua/plugin/mini.lua
+++ lua/plugin/mini.lua
@@ -2,9 +2,29 @@ return {
     {
         "echasnovski/mini.comment",
         opts = {
-            options = { ignore_blank_line = true },
             mappings = { comment_line = "gcc" },
+            options = { ignore_blank_line = true },
         },
         event = { "BufReadPost", "BufNewFile" },
+    },
+    {
+        "echasnovski/mini.move",
+        opts = {
+            mappings = {
+                up = "<M-Up>",
+                down = "<M-Down>",
+                left = "<M-Left>",
+                right = "<M-Right>",
+
+                -- Move current line in Normal mode
+                line_up = "<M-Up>",
+                line_down = "<M-Down>",
+                line_left = "<M-Left>",
+                line_right = "<M-Right>",
+            },
+            options = {
+                reindent_lineise = true,
+            },
+        },
     },
 }
