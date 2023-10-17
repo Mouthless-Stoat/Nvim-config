9187a83b9241e0bf1883cf0cf44dda7d5dedc6ef
khanhfg <nphuyanh@gmail.com>
2023-10-16 22:46:19 -0300

    Fix surroun not being set up

--- lua/plugin/surround.lua
+++ lua/plugin/surround.lua
@@ -1,5 +1,6 @@
 return {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    opts = {},
     event = { "BufReadPost", "BufNewFile" },
 }
