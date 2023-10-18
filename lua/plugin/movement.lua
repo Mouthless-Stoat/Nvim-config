71968848e05dd4d1c3d6a97c0e90c85795e6c916
khanhfg <nphuyanh@gmail.com>
2023-10-18 18:24:45 -0300

    More hop movement

--- lua/plugin/movement.lua
+++ lua/plugin/movement.lua
@@ -9,14 +9,37 @@ return {
                 function()
                     require("hop").hint_vertical()
                 end,
+                desc = "[h]op [l]ine",
             },
             {
                 "<leader>hw",
                 function()
+                    ---@diagnostic disable-next-line: missing-parameter
                     require("hop").hint_words()
                 end,
+                desc = "[h]op [w]ord",
             },
-            { "<leader>h1" },
+            {
+                "<leader>h1",
+                function()
+                    require("hop").hint_char1()
+                end,
+                desc = "[h]op [1] character",
+            },
+            {
+                "<leader>h2",
+                function()
+                    require("hop").hint_char2()
+                end,
+                desc = "[h]op [2] character",
+            },
+            {
+                "<leader>hp",
+                function()
+                    ---@diagnostic disable-next-line: missing-parameter
+                    require("hop").hint_patterns()
+                end,
+            },
         },
         event = { "BufReadPost", "BufNewFile" },
     },
