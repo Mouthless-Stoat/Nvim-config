0cac1a10565afceeaa1e6955d09f2473a8a1cd65
khanhfg <nphuyanh@gmail.com>
2023-10-18 14:59:21 -0300

    Add in hop movement

--- lua/plugin/movement.lua
+++ lua/plugin/movement.lua
@@ -1,3 +1,23 @@
 return {
-    { "smoka7/hop.nvim", version = "*", opts = {}, event = { "BufReadPost", "BufNewFile" } },
+    {
+        "smoka7/hop.nvim",
+        version = "*",
+        opts = {},
+        keys = {
+            {
+                "<leader>hl",
+                function()
+                    require("hop").hint_vertical()
+                end,
+            },
+            {
+                "<leader>hw",
+                function()
+                    require("hop").hint_words()
+                end,
+            },
+            { "<leader>h1" },
+        },
+        event = { "BufReadPost", "BufNewFile" },
+    },
 }
