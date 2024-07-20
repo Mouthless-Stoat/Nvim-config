c04499e88d0e4e5227f230245c29523068e7df45
Mouthless Stoat <nphuyanh@outlook.com>
2024-07-20 10:42:20 -0300

    Change screen key config

--- lua/plugins/screenkey.lua
+++ lua/plugins/screenkey.lua
@@ -4,9 +4,18 @@ return {
     version = "*", -- or branch = "dev", to use the latest commit
 
     opts = {
+        clear_after = 100000,
+        group_mappings = true,
+        show_leader = false,
         win_opts = {
-            border = "rounded",
-            title = " Screenkey ",
+            row = 2,
+            col = vim.o.columns - 1,
+            anchor = "NE",
+
+            border = "solid",
+            title = " screenkey ",
+            height = 1,
+            width = 30,
         },
     },
 }
