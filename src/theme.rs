4a8401629eee32fb1f0e127d6203740d1d9cf11c
Mouthless Stoat <nphuyanh@outlook.com>
2025-07-20 12:06:12 -0300

    feat(theme): change syntax highlight style in theme

--- src/theme.rs
+++ src/theme.rs
@@ -8,7 +8,7 @@ pub fn configure_highlight() -> nvim_oxi::Result<()> {
 
 #[rustfmt::skip]
 fn nvim_highlight() -> nvim_oxi::Result<()> {
-    set_hl( "Normal", HighlightOpt::new().fg(Color::White).bg(Color::Bg0))?;
+    set_hl( "Normal", HighlightOpt::new().fg(Color::White).bg(Color::Bg1))?;
 
     set_hl("NCursor", HighlightOpt::new().bg(Color::Blue))?;
     set_hl("ICursor", HighlightOpt::new().bg(Color::Green))?;
@@ -21,31 +21,31 @@ fn nvim_highlight() -> nvim_oxi::Result<()> {
     set_hl("WarningMsg", HighlightOpt::new().fg(Color::Yellow))?;
     set_hl("Question", HighlightOpt::new().fg(Color::Green))?;
 
-    set_hl("IncSearch", HighlightOpt::new().fg(Color::Bg0).bg(Color::Yellow))?;
-    set_hl("Subsitube", HighlightOpt::new().fg(Color::Bg0).bg(Color::Green))?;
+    set_hl("IncSearch", HighlightOpt::new().fg(Color::Bg1).bg(Color::Yellow))?;
+    set_hl("Subsitube", HighlightOpt::new().fg(Color::Bg1).bg(Color::Green))?;
 
     set_hl("Yank", HighlightOpt::new().reverse())?;
 
-    set_hl("Visual", HighlightOpt::new().fg(Color::Bg0).bg(Color::Purple))?;
-    set_hl("EndOfBuffer", HighlightOpt::new().fg(Color::Bg0))?;
+    set_hl("Visual", HighlightOpt::new().fg(Color::Bg1).bg(Color::Purple))?;
+    set_hl("EndOfBuffer", HighlightOpt::new().fg(Color::Bg1))?;
 
-    set_hl("LineNr", HighlightOpt::new().fg(Color::Gray).bg(Color::Bg0))?;
-    set_hl("CursorLineNr", HighlightOpt::new().fg(Color::Blue).bg(Color::Bg0))?;
+    set_hl("LineNr", HighlightOpt::new().fg(Color::Gray).bg(Color::Bg2))?;
+    set_hl("CursorLineNr", HighlightOpt::new().fg(Color::Blue).bg(Color::Bg1))?;
 
-    set_hl("DiffAdd", HighlightOpt::new().fg(Color::Green).bg(Color::Bg0).bold().italic())?;
-    set_hl("DiffChange", HighlightOpt::new().fg(Color::Yellow).bg(Color::Bg0).bold().italic())?;
-    set_hl("DiffDelete", HighlightOpt::new().fg(Color::Red).bg(Color::Bg0).bold().italic(),)?;
-    set_hl("DiffText", HighlightOpt::new().fg(Color::Blue).bg(Color::Bg0).bold().italic())?;
+    set_hl("DiffAdd", HighlightOpt::new().fg(Color::Green).bg(Color::Bg1).bold().italic())?;
+    set_hl("DiffChange", HighlightOpt::new().fg(Color::Yellow).bg(Color::Bg1).bold().italic())?;
+    set_hl("DiffDelete", HighlightOpt::new().fg(Color::Red).bg(Color::Bg1).bold().italic(),)?;
+    set_hl("DiffText", HighlightOpt::new().fg(Color::Blue).bg(Color::Bg1).bold().italic())?;
 
     set_hl("Changed", HighlightOpt::new().fg(Color::Yellow).bold())?;
     set_hl("Added", HighlightOpt::new().fg(Color::Green).bold())?;
     set_hl("Removed", HighlightOpt::new().fg(Color::Red).bold())?;
 
-    set_hl("WinSeparator", HighlightOpt::new().fg(Color::Blue).bg(Color::Bg1))?;
+    set_hl("WinSeparator", HighlightOpt::new().fg(Color::Blue).bg(Color::Bg2))?;
 
-    set_hl("MatchParen", HighlightOpt::new().fg(Color::Blue).bg(Color::Bg1))?;
+    set_hl("MatchParen", HighlightOpt::new().fg(Color::Blue).bg(Color::Bg2))?;
 
-    set_hl("MsgArea", HighlightOpt::new().fg(Color::Yellow).bg(Color::Bg0))?;
+    set_hl("MsgArea", HighlightOpt::new().fg(Color::Yellow).bg(Color::Bg1))?;
 
     set_hl("SpellBad", HighlightOpt::new().fg(Color::Red))?;
     set_hl("SpellCap", HighlightOpt::new().fg(Color::Blue))?;
@@ -86,22 +86,21 @@ macro_rules! colors {
 }
 
 colors! {
-    Pink = "#ff0084";
+    Pink = "#ff4f9b";
     Red = "#f65866";
     Orange = "#fa9534";
     Yellow = "#efbd5d";
     Green = "#8bcd5b";
-    Cyan = "#34bfd0";
+    Cyan = "#00b8b8";
     Blue = "#41a7fc";
     Purple = "#c75ae8";
 
-    White = "#93a4c3";
-    Gray = "#455574";
-    Bg0 = "#020a20"; // darkest
-    Bg1 = "#09152c";
-    Bg2 = "#142139";
-    Bg3 = "#202e47"; // lightest
-
+    White = "#829bcd";
+    Gray = "#68687a";
+    Bg0 = "#16161D"; // darkest
+    Bg1 = "#1F1F28"; // default bg
+    Bg2 = "#2A2A37";
+    Bg3 = "#363646"; // lightest
 }
 
 // Not using SetHighlightOpts by nvim_oxi because it is too complex with too many feature that we
