231196e801a731550305fe5dd25163e069d25dab
Mouthless Stoat <nphuyanh@outlook.com>
2025-07-21 14:33:36 -0300

    feat(treesitter): add treesitter for better syntax highlighting

--- src/lazy.rs
+++ src/lazy.rs
@@ -1,3 +1,5 @@
+use crate::lua_table;
+
 mod plugins;
 
 use plugins::*;
@@ -6,6 +8,22 @@ pub fn setup_lazy() -> nvim_oxi::Result<()> {
     let mut lazy = Lazy::new();
 
     lazy.add_plugin("neovim/nvim-lspconfig");
+
+    lazy.add_plugin(
+        LazyPlugin::new("nvim-treesitter/nvim-treesitter")
+            .main("nvim-treesitter.configs")
+            .version(LazyVersion::Branch("master"))
+            .build(":TSUpdate")
+            .opts(lua_table! {
+                ensure_installed = ["python", "javascript", "typescript", "rust"],
+                auto_install = true,
+                highlight = lua_table! {
+                    enable = true,
+                    additional_vim_regex_highlighting = false
+                },
+                indent = lua_table! { enable = true }
+            }),
+    );
 
     lazy.setup()
 }
