9685217bda554a5aa07d3b065d5adb6ee40178e4
Mouthless Stoat <nphuyanh@outlook.com>
2025-07-20 21:59:19 -0300

    feat(lazy): new lazy setup and configuration scheme
    Create a new Lazy instance add plugins with add_plugin and finally call
    setup to bootstrap and set up all plugins.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use mlua::{Function, Table};
+
 mod lazy;
 mod lsp;
 mod macros;
@@ -10,6 +12,18 @@ fn config() -> nvim_oxi::Result<()> {
     lsp::setup_lsp()?;
     options::configure_options()?;
     theme::configure_highlight()?;
+
+    Ok(())
+}
+
+/// Helper for require and setup method
+pub fn require_setup(module: &str, opts: impl mlua::IntoLua) -> nvim_oxi::Result<()> {
+    nvim_oxi::mlua::lua()
+        .globals()
+        .get::<Function>("require")?
+        .call::<Table>(module)?
+        .get::<Function>("setup")?
+        .call::<()>(opts)?;
 
     Ok(())
 }
