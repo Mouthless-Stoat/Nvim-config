f51a09b419c26feaa9c2dbf5dd2251545b63e0e6
Mouthless Stoat <nphuyanh@outlook.com>
2025-07-21 22:23:02 -0300

    feat(keymap): add configuration for keymaps

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,25 @@
 use mlua::{Function, Table};
+use nvim_oxi::api::types::Mode as OxiMode;
 
+mod keymaps;
 mod lazy;
 mod lsp;
 mod macros;
 mod options;
 mod theme;
+
+#[nvim_oxi::plugin]
+fn config() -> nvim_oxi::Result<()> {
+    lazy::setup_lazy()?;
+    lsp::setup_lsp()?;
+
+    options::configure_options()?;
+    keymaps::configure_keymaps()?;
+
+    theme::configure_highlight()?;
+
+    Ok(())
+}
 
 #[derive(Clone, Copy)]
 pub enum Mode {
@@ -24,7 +39,7 @@ impl Mode {
             "v" | "vs" | "V" | "Vs" | "\u{16}" | "\u{16}s" | "s" | "S" | "\u{13}" => Self::Visual,
             "c" | "cv" | "ce" | "rm" | "r?" => Self::Command,
             "R" | "Rc" | "Rx" | "Rv" | "Rvc" | "Rvx" | "r" => Self::Replace,
-            _ => Self::Terminal
+            _ => Self::Terminal,
         })
     }
 
@@ -35,19 +50,22 @@ impl Mode {
             Self::Command => "command",
             Self::Visual => "visual",
             Self::Replace => "replace",
-            Self::Terminal => "terminal"
+            Self::Terminal => "terminal",
         }
     }
 }
 
-#[nvim_oxi::plugin]
-fn config() -> nvim_oxi::Result<()> {
-    lazy::setup_lazy()?;
-    lsp::setup_lsp()?;
-    options::configure_options()?;
-    theme::configure_highlight()?;
-
-    Ok(())
+impl From<Mode> for OxiMode {
+    fn from(value: Mode) -> Self {
+        match value {
+            Mode::Normal => OxiMode::Normal,
+            Mode::Insert => OxiMode::Insert,
+            Mode::Command => OxiMode::CmdLine,
+            Mode::Visual => OxiMode::Visual,
+            Mode::Terminal => OxiMode::Terminal,
+            Mode::Replace => panic!("You can't convert from replace mode"),
+        }
+    }
 }
 
 /// Helper for require and setup method
