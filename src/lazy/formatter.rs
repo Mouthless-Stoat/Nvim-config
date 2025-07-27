705e75829e19100fdf503e071a3113504141975d
Mouthless Stoat <nphuyanh@outlook.com>
2025-07-26 23:58:18 -0300

    style: reformat with `cargo fmt`

--- src/lazy/formatter.rs
+++ src/lazy/formatter.rs
@@ -5,23 +5,21 @@ use crate::{autocmds::create_autocmd, require, require_setup, table};
 use super::LazyPlugin;
 
 pub fn plugin() -> nvim_oxi::Result<LazyPlugin> {
-    Ok(LazyPlugin::new("stevearc/conform.nvim")
-        .opts(table! {
-            formatters_by_ft = table! {
-                lua = ["stylua"], 
-                python = ["black"], 
-                javascript = ["prettier"], 
-                typescript = ["prettier"], 
-                json = ["prettier"], 
-                markdown = ["prettier"], 
-                rust = ["rustfmt"], 
-                yaml = ["prettier"], 
-                toml = ["taplo"] 
-            },
-            format_on_save = table! {
-                timeout_ms = 500,
-                lsp_format = "fallback"
-            }
-        }) 
-        )
+    Ok(LazyPlugin::new("stevearc/conform.nvim").opts(table! {
+        formatters_by_ft = table! {
+            lua = ["stylua"],
+            python = ["black"],
+            javascript = ["prettier"],
+            typescript = ["prettier"],
+            json = ["prettier"],
+            markdown = ["prettier"],
+            rust = ["rustfmt"],
+            yaml = ["prettier"],
+            toml = ["taplo"]
+        },
+        format_on_save = table! {
+            timeout_ms = 500,
+            lsp_format = "fallback"
+        }
+    }))
 }
