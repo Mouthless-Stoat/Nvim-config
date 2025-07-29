d194f3cce70011d2d40965e20d94d15dddb048a0
Mouthless Stoat <nphuyanh@outlook.com>
2025-07-29 16:58:48 -0300

    fix: change compiling config to be more specific

--- init.lua
+++ init.lua
@@ -6,14 +6,14 @@ local rust_config = vim.fs.joinpath(config_path, "/lua", "/config.dll")
 
 if WIP then
     -- TODO: replace config.dll with platform specific ending
-    vim.fs.rm(rust_config, {force=true})
+    vim.fs.rm(rust_config, { force = true })
 end
 
 -- compile rust config if file not found
 if not vim.uv.fs_stat(rust_config) then
     local cwd = vim.fn.getcwd()
     vim.cmd.cd(config_path)
-    vim.fn.system("just")
+    vim.fn.system("just build")
     vim.cmd.cd(cwd)
 end
 require("config")
